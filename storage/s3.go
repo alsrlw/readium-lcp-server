1b1cb4ea1eee33ff90d82e778c58fc29d98993cd
Robert L. Williams <williams@amigos.org>
2020-07-20 16:51:43 -0500

    Remove S3Config Token value as a requirement; make it an optionally supplied component

--- storage/s3.go
+++ storage/s3.go
@@ -135,8 +135,12 @@ func S3(config S3Config) (Store, error) {
 	// Credentials defaults to a chain of credential providers to search for credentials in environment
 	// variables, shared credential file, and EC2 Instance Roles.
 	// Therefore, we only explicitly define static credentials if these are present in config
-	if config.ID != "" && config.Secret != "" && config.Token != "" {
-		awsConfig.Credentials = credentials.NewStaticCredentials(config.ID, config.Secret, config.Token)
+	if config.ID != "" && config.Secret != "" {
+    	if config.Token != "" {
+    		awsConfig.Credentials = credentials.NewStaticCredentials(config.ID, config.Secret, config.Token)
+        } else {
+    		awsConfig.Credentials = credentials.NewStaticCredentials(config.ID, config.Secret, "")
+        }
 	}
 
 	return &s3store{client: s3.New(session.New(awsConfig)), bucket: config.Bucket}, nil
