4de26606b1e559cac9b0608cb29523422007ffc5
Robert Williams <williams@amigos.org>
2020-07-25 17:40:38 -0500

    Add temporary logging to session creation function to be sure the value transfers; revised session creation code, with error

--- storage/s3.go
+++ storage/s3.go
@@ -126,22 +126,28 @@ type S3Config struct {
 
 // S3 inits and S3 storage
 func S3(config S3Config) (Store, error) {
-	awsConfig := &aws.Config{
-		DisableSSL:                    aws.Bool(config.DisableSSL),
-		S3ForcePathStyle:              aws.Bool(config.ForcePathStyle),
-		Region:                        aws.String(config.Region),
-		Endpoint:                      aws.String(config.Endpoint)}
+//	awsConfig := &aws.Config{
+//		DisableSSL:                    aws.Bool(config.DisableSSL),
+//		S3ForcePathStyle:              aws.Bool(config.ForcePathStyle),
+//		Region:                        aws.String(config.Region),
+//		Endpoint:                      aws.String(config.Endpoint)}
 
 	// Credentials defaults to a chain of credential providers to search for credentials in environment
 	// variables, shared credential file, and EC2 Instance Roles.
 	// Therefore, we only explicitly define static credentials if these are present in config
 	if config.ID != "" && config.Secret != "" {
-    	if config.Token != "" {
-    		awsConfig.Credentials = credentials.NewStaticCredentials(config.ID, config.Secret, config.Token)
-        } else {
-    		awsConfig.Credentials = credentials.NewStaticCredentials(config.ID, config.Secret, "")
-        }
+		log.Println("S3 module config ID value: z" + config.ID + "z")
+		sess, err := session.NewSession(&aws.Config{
+			DisableSSL:                    aws.Bool(config.DisableSSL),
+			S3ForcePathStyle:              aws.Bool(config.ForcePathStyle),
+			Region:                        aws.String(config.Region),
+			Credentials:                   credentials.NewStaticCredentials(config.ID, config.Secret, config.Token),
+			})
+		// awsConfig.Credentials = credentials.NewStaticCredentials(config.ID, config.Secret, config.Token)
+		if err != nil {
+			panic(err)
+		}	
 	}
 
-	return &s3store{client: s3.New(session.New(awsConfig)), bucket: config.Bucket}, nil
+	return &s3store{client: s3.New(sess), bucket: config.Bucket}, nil
 }
